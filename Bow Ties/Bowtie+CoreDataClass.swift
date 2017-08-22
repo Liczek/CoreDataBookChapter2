ca032115bce22221c16b027217c6e22189d40b9e
Paweł Liczmański <liczek@MacBook-Pro-Pawe.local>
2017-08-22 23:19:32 +0200

    żeby udało się skompilować po stworzeniu plików NS... w entitie było trzeba zmienić Module na Current Product Module i Codegen Manual/None

---
+++ Bow Ties/Bowtie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Paweł Liczmański on 22.08.2017.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Bowtie: NSManagedObject {
+
+}
